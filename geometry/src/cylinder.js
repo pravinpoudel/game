c1b0857d491b94519378ef8a07767c927849981e
alok <alksbd9@gmail.com>
2021-06-06 18:31:40 +0545

    Create CyclinderGeometry

---
+++ geometry/src/cylinder.js
@@ -0,0 +1,29 @@
+export default class CircleGeometry {
+    constructor(radius, height) {
+        let verticesCount = 16;
+        let alpha = (2 * Math.PI) / verticesCount;
+        let vertices = [];
+        let indices = [];
+
+        for (var i = 0; i < verticesCount; i++) {
+            let x = radius * Math.cos(i * alpha);
+            let z = radius * Math.sin(i * alpha);
+            vertices.push(x, height/2, z);
+            vertices.push(x, -height/2, z);
+        }
+
+
+        for (var i = 5; i < verticesCount*2; i += 2) {
+            indices.push(0, i-3, i-1);
+            indices.push(1, i-2, i);
+
+            indices.push(i-5, i-4, i-3);
+            indices.push(i-4, i-3, i-2);
+        }
+        indices.push((verticesCount*2)-2, (verticesCount*2)-1, 0);
+        indices.push((verticesCount*2)-1, 0, 1);
+
+        indices.push((verticesCount*2)-4, (verticesCount*2)-3, (verticesCount*2)-2);
+        indices.push((verticesCount*2)-4, (verticesCount*2)-2, (verticesCount*2)-1);
+    }
+}
