da2a23a0a5fb7e1b1794296363cecd002af3cb5c
alok <alksbd9@gmail.com>
2021-06-09 12:53:04 +0545

    Collision test for box against sphere

--- physics/src/collision.js
+++ physics/src/collision.js
@@ -4,4 +4,16 @@ export default class Collision {
             (box1.minY <= box2.maxY && box1.maxY >= box2.minY) &&
             (box1.minZ <= box2.maxZ && box1.maxZ >= box2.minZ);
     }
+
+    collidedBoxToSphere(box, sphere) {
+        var x = Math.max(box.minX, Math.min(sphere.x, box.maxX));
+        var y = Math.max(box.minY, Math.min(sphere.y, box.maxY));
+        var z = Math.max(box.minZ, Math.min(sphere.z, box.maxZ));
+
+        var distance = Math.sqrt((x - sphere.x) * (x - sphere.x) +
+                                (y - sphere.y) * (y - sphere.y) +
+                                (z - sphere.z) * (z - sphere.z));
+
+        return distance < sphere.radius;
+    }
 }
