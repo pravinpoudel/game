479e5700ef14ac815d885d052735bfbca891b096
alok <alksbd9@gmail.com>
2021-06-11 13:57:35 +0545

    Gameobject can move by given values

--- gameObject/src/gameObject.js
+++ gameObject/src/gameObject.js
@@ -17,4 +17,10 @@ export default class GameObject {
             z: 0,
         }
     }
+
+    moveBy(x, y, z) {
+        this.position.x += x
+        this.position.y += y
+        this.position.z += z
+    }
 }
--- gameObject/tests/gameObject.test.js
+++ gameObject/tests/gameObject.test.js
@@ -15,3 +15,13 @@ test('GameObject can have geometry', () => {
     expect(new GameObject().geometry).toBeNull();
     expect(new GameObject(expectedGeometry).geometry).toEqual(expectedGeometry);
 });
+
+test('GameObject move by given value', () => {
+    let expectedPosition1 = {x: 1, y: -1.5, z:2}
+    let expectedPosition2 = {x: 2, y: 0, z:1}
+    let gameObject = new GameObject()
+    gameObject.moveBy(1, -1.5, 2)
+    expect(gameObject.position).toEqual(expectedPosition1);
+    gameObject.moveBy(1, 1.5, -1)
+    expect(gameObject.position).toEqual(expectedPosition2);
+});
